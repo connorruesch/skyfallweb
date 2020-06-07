61d4fa281f55d77e0ff551a8bb91f23f609d5942
Baricaade <baricaade@gmail.com>
2020-06-06 22:22:35 -0700

    fix navbar

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,28 @@
 <template>
-  <div id="app">
-    <!--Navigation-->
-    <b-navbar class="fixed-top" toggleable="md" type="dark">
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="links">
-          <b-nav-item class="padding" to="/">Home</b-nav-item>
-          <b-nav-item class="padding" to="/">Rules</b-nav-item>
-          <b-nav-item class="padding" to="/">Play</b-nav-item>
-          <b-nav-item class="padding" to="/">Announcements</b-nav-item>
-        </b-navbar-nav>
-
-        <a href="#" class="ml-auto discord">
-          <b-navbar-nav class="discord-icon">
-            <i class="fab fa-discord"></i>
-            <p>{{ discordOnline }} Online Members!</p>
-          </b-navbar-nav>
-        </a>
-      </b-collapse>
-    </b-navbar>
-    <router-view />
-  </div>
+    <div id="app">
+        <Navbar v-on:scroll.native="handleScroll" />
+        <router-view />
+    </div>
 </template>
 
 <script>
+import Navbar from "../src/components/Navbar";
+
 export default {
-  name: "App",
-  data() {
-    return {
-      discordOnline: 0
-    };
-  },
-  created() {
-    fetch(
-      "https://ptb.discordapp.com/api/guilds/716012002748792972/widget.json"
-    )
-      .then(res => res.json())
-      .then(resData => (this.discordOnline = resData.presence_count))
-      .catch(err => console.log(err));
-  }
+    name: "App",
+    components: {
+        Navbar
+    },
+    methods: {
+        handleScroll() {
+            const navbar = document.querySelector(".navigation");
+            if (window.scrollY > 50) {
+                navbar.classList.add("navScroll");
+            } else {
+                navbar.classList.remove("navScroll");
+            }
+        }
+    }
 };
 </script>
 
@@ -52,76 +35,8 @@ $font-stack: Arial, Helvetica, sans-serif;
 html,
 body,
 * {
-  padding: 0;
-  margin: 0;
-  font-family: $font-stack;
-}
-
-.navbar {
-  padding: 1rem 1.5rem 1rem 2rem;
-  text-decoration: none;
-  z-index: 1;
-
-  .links {
-    padding-left: 2rem;
-  }
-
-  .padding {
-    margin-right: 1rem;
-  }
-
-  .nav-link {
-    color: white !important;
-    position: relative;
-    text-decoration: none;
-    font-weight: 700;
-    font-size: 1.2rem;
-    text-transform: uppercase;
-  }
-
-  /*Navigation link hover effects*/
-  .nav-link:hover {
-    color: white;
-  }
-
-  .nav-link::after {
-    content: "";
-    position: absolute;
-    height: 6px;
-    width: 100%;
-    top: -40%;
-    left: 0%;
-    background: white;
-    transform: scaleX(0);
-    transform-origin: center;
-    transition: transform 0.18s;
-  }
-
-  .nav-link:hover::after {
-    transform: scaleX(1);
-  }
-
-  .discord {
-    color: white;
-    padding-right: 2rem;
-    text-decoration: none;
-
-    i {
-      font-size: 35px;
-      padding: 0.7rem;
-    }
-
-    p {
-      padding-top: 1rem;
-      font-size: 1.06rem;
-      font-weight: 600;
-      text-transform: uppercase;
-    }
-
-    :hover {
-      color: #7289da !important;
-      transition: 0.3s ease-in-out;
-    }
-  }
+    padding: 0;
+    margin: 0;
+    font-family: $font-stack;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,121 @@
+<template>
+    <!--Navigation-->
+    <b-navbar class="navigation" toggleable="md" type="dark">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="links">
+                <b-nav-item class="padding" to="/">Home</b-nav-item>
+                <b-nav-item class="padding" to="/">Rules</b-nav-item>
+                <b-nav-item class="padding" to="/">Play</b-nav-item>
+                <b-nav-item class="padding" to="/">Announcements</b-nav-item>
+            </b-navbar-nav>
+
+            <a href="#" class="ml-auto discord">
+                <b-navbar-nav class="discord-icon">
+                    <i class="fab fa-discord"></i>
+                    <p>{{ discordOnline }} Online Members!</p>
+                </b-navbar-nav>
+            </a>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+    data() {
+        return {
+            discordOnline: 0
+        };
+    },
+    created() {
+        fetch(
+            "https://ptb.discordapp.com/api/guilds/716012002748792972/widget.json"
+        )
+            .then(res => res.json())
+            .then(resData => (this.discordOnline = resData.presence_count))
+            .catch(err => console.log(err));
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+/* Global Navbar */
+.navbar {
+    padding: 1rem 1.5rem 1rem 2rem;
+    text-decoration: none;
+    z-index: 1;
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+
+    .links {
+        padding-left: 2rem;
+    }
+
+    .padding {
+        margin-right: 1rem;
+    }
+
+    .nav-link {
+        color: white !important;
+        position: relative;
+        text-decoration: none;
+        font-weight: 700;
+        font-size: 1.2rem;
+        text-transform: uppercase;
+    }
+
+    .navScroll {
+        background-color: rgba(0, 0, 0, 0.6);
+        transition: 1s;
+    }
+
+    /*Navigation link hover effects*/
+    .nav-link:hover {
+        color: white;
+    }
+
+    .nav-link::after {
+        content: "";
+        position: absolute;
+        height: 6px;
+        width: 100%;
+        top: -53%;
+        left: 0%;
+        background: white;
+        transform: scaleX(0);
+        transform-origin: center;
+        transition: transform 0.18s;
+    }
+
+    .nav-link:hover::after {
+        transform: scaleX(1);
+    }
+
+    .discord {
+        color: white;
+        padding-right: 2rem;
+        text-decoration: none;
+
+        i {
+            font-size: 35px;
+            padding: 0.7rem;
+        }
+
+        p {
+            padding-top: 1rem;
+            font-size: 1.06rem;
+            font-weight: 600;
+            text-transform: uppercase;
+        }
+
+        :hover {
+            color: #7289da !important;
+            transition: 0.3s ease-in-out;
+        }
+    }
+}
+</style>
