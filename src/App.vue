8b47537a40526cfc511856828a0042a036c3d8cc
Baricaade <baricaade@gmail.com>
2020-06-07 20:35:33 -0700

    fix meta

--- src/App.vue
+++ src/App.vue
@@ -16,36 +16,6 @@ export default {
     metaInfo() {
         return {
             title: 'SkyFall Network',
-            meta: [
-                {
-                    name: 'language',
-                    content: 'english',
-                },
-                {
-                    name: 'author',
-                    content: 'SkyFall Network',
-                },
-                {
-                    name: 'designer',
-                    content: 'Baricaade',
-                },
-                {
-                    name: 'description',
-                    content: 'The SkyFall Network is a minecraft network consisting of two gamemodes: skyblock and eco-based survival. We are focused on giving players a fun and play-2-win experience, with special features such as an ingame store, playtime rewards, and more.',
-                },
-                {
-                    name: 'keywords',
-                    content: 'minecraft,play2win,non-pay2win,network,server,1.8.9,skyfall,skyfallnetwork',
-                },
-                {
-                    name: 'copyright',
-                    content: 'Copyright 2020 | SkyFall Network',
-                },
-                {
-                    property: 'og:image',
-                    content: '../assets/iconasset.png',
-                },
-            ],
         };
     },
 };
